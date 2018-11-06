8a1f5b654e7277ff2e6d2d453b192e04682003f2
georgelevov <georgelevov@gmail.com>
2018-11-06 13:23:06 +0200

    user-model-update

--- public/src/model/user.ts
+++ public/src/model/user.ts
@@ -1,52 +1,21 @@
 import { UserType } from './../enums/user-type';
 import { IUser } from '../contracts/models';
 export class User implements IUser {
-    name: string;
-    password: string;
-    userType: UserType;
 
 constructor(
     private _name: string,
     private _password: string,
-    private _userType: UserType = UserType.User ) 
-    
-    {
+    private _userType: UserType = UserType.User )     {    }
 
-    
-    }
-
-
-
-
-
-
-
-
-
+public get name():string{
+    return this._name;
+}
+public get password():string{
+    return this._password;
+}
+public get userType():UserType{
+    return this._userType;
+}
 
 }
 
-
-// @injectable()
-// export class CreateUser implements ICommand {
-//   private readonly _data: IFuritureDatabase;
-//   private readonly _factory: IModelsFactory;
-
-//   public constructor(@inject(TYPES.furnitureDatabase) data: IFuritureDatabase, @inject(TYPES.modelsFactory) factory: IModelsFactory) {
-//     this._data = data;
-//     this._factory = factory;
-//   }
-
-//   public execute(parameters: string[]): string {
-//     const [companyName, companyRegistrationNumber] = parameters;
-
-//     if (this._data.companies.find((company: ICompany) => company.name === companyName)) {
-//       throw new Error(Constants.getCompanyExistsErrorMessage(companyName));
-//     }
-
-//     const newCompany: ICompany = this._factory.createCompany(companyName, companyRegistrationNumber);
-//     this._data.companies.push(newCompany);
-
-//     return Constants.getCompanyCreatedSuccessMessage(companyName);
-//   }
-// }
