df55d7fe83dea711f1a7b5139687a2448edda5f2
jibonita <bamishore@gmail.com>
2018-11-09 15:08:40 +0200

    Stefi folder sunced with Miro's changes

--- testing/stefi/src/contracts/models/book.ts
+++ testing/stefi/src/contracts/models/book.ts
@@ -1,10 +1,7 @@
 import { BookGenre } from '../../enums/book-genre';
 export interface IBook {
-  title: string;
-  author: string;
-  genre:BookGenre;
-  //availability: boolean;
- // If partOfSeries is true we need to implement ISeries
-
+    title: string;
+    author: string;
+    genre:BookGenre;
 
 }
--- testing/stefi/src/models/book.ts
+++ testing/stefi/src/models/book.ts
@@ -1,27 +1,40 @@
 import { IBook } from "../contracts/models/book";
 import { BookGenre } from "../enums/book-genre";
+
 export class Book implements IBook {
+    private readonly _title: string;
+    private readonly _author: string;
+    private readonly _genre: BookGenre;
+    public static bookId:number = 0;
+    public id:number;
 
+    public constructor(title: string, author: string, genre: BookGenre) {
+        if (title.length < 2 || !(title.search(/[^A-Za-z][0-9]/))) {
+            throw new Error("Book title must be at less 2 characters long!");
+        }
+        if (author.length < 3 || !(author.search(/[^A-Za-z][0-9]/))) {
+                    throw new Error("The authors name must be at less characters long!");
+                }
+        if (!BookGenre[genre]) {
+            throw new Error("Invalid book genre");
+        }
 
-constructor(private _title: string,
-     private _author: string,
-      private _genre: BookGenre,
-       ) {
+        this._title = title;
+        this._author = author;
+        this._genre = genre;
+        Book.bookId += 1;
+        this.id = Book.bookId;
 
-        // set this._....
-}
+    }
 
-public get title(): string{
-    return this._title;
-}; 
-public get author(): string{
-    return this._author;
-};
-public get genre(): BookGenre{
-    return this._genre;
-};
-
-
-
-
-}
+    public get title(): string {
+        return this._title;
+    }
+    public get author(): string {
+        return this._author;
+    }
+    public get genre(): BookGenre {
+        return this._genre;
+    }
+        
+    }
